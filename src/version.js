05f7248e5d410551c27d1c171217731338f7153c
sergey.vladimirov <sergey.vladimirov@redsolution.ru>
2017-09-04 17:56:17 +0500

    Make version (1.0.2)

--- src/version.js
+++ src/version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"1.0.1","version_description":"Fixed minor bugs in user interface."}'
+'{"version_number":"1.0.2","version_description":"Revised mechanism of accounts synchronization."}'
 )});
