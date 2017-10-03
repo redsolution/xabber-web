0fdcb4cdae3cb72fbcd29a051da7ec02f0460674
sergey.vladimirov <sergey.vladimirov@redsolution.ru>
2017-10-03 17:30:00 +0500

    Make version (1.0.8)

--- src/version.js
+++ src/version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"1.0.7","version_description":"Improved mechanism of synchronization with Xabber account"}'
+'{"version_number":"1.0.8","version_description":"Implemented settings synchronization via push notifications. Fixed bugs with offline status and quit function."}'
 )});
