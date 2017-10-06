0ceb0b282f31e522ad467e9b12ce47af63e1b44a
sergey.vladimirov <sergey.vladimirov@redsolution.ru>
2017-10-06 17:46:05 +0500

    Make version (1.0.10)

--- src/version.js
+++ src/version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"1.0.9","version_description":"Fixed work in iOS Safari browser"}'
+'{"version_number":"1.0.10","version_description":"Fixed work in iOS Safari browser"}'
 )});
