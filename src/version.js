43a6405ecd2d9cf13a0c24e272d037a9ef62e4d5
Valeria Nogina <valeria.nogina@redsolution.ru>
2018-04-26 16:01:35 +0500

    Make version (1.0.13)

--- src/version.js
+++ src/version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"1.0.12","version_description":"Add source info for Xabber API calls"}'
+'{"version_number":"1.0.13","version_description":"1.0.13"}'
 )});
