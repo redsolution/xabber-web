f81e716746188ed0ae8896eff6013f9bb4fd5487
Valeria Nogina <valeria.nogina@redsolution.ru>
2018-10-23 11:46:50 +0500

    Make version (1.0.15)

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"1.0.14","version_description":"Add voice messages. Add support of XEP-0012: Last Activity."}'
+'{"version_number":"1.0.15","version_description":"Improved Xabber Account"}'
 )});
