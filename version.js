67adda6ee530d53514e718c26476e20f7f95a617
Valeria Nogina <valeria.nogina@redsolution.ru>
2018-11-13 09:26:17 +0500

    Fixed bug at loading Xabber

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"1.0.17","version_description":"Improved group chats: implemented all messages and messages of user retraction.\nAdded error exception messages."}'
+'{"version_number":"1.0.17","version_description":"Improved group chats: implemented all messages and messages of user retraction. Added error exception messages."}'
 )});
