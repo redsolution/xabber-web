a479e028811b1757936d4ff30cb6fb665657e273
Valeria Nogina <valeria.nogina@redsolution.ru>
2019-09-25 13:12:32 +0500

    Make version 2.0.2

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.0.1","version_description":"Implemented group chats, client synchronization, VoIP-calls"}'
+'{"version_number":"2.0.2","version_description":"Implemented group chats, client synchronization, VoIP-calls"}'
 )});
