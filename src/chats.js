ba938be24b0404db0de851481beeb5521c24f728
Valeria Nogina <valeria.nogina@redsolution.ru>
2019-11-22 12:25:17 +0500

    Fixed message parsing createFromStanza

--- src/chats.js
+++ src/chats.js
@@ -293,11 +293,11 @@ define("xabber-chats", function () {
                 message = msgid && this.get(msgid);
 
             if (options.replaced) {
-                if (!message)
-                    return;
                 $message = $message.find('replace message');
                 body = $message.children('body').text();
                 message = this.find(m => m.get('archive_id') === $message.children('stanza-id').attr('id'));
+                if (!message)
+                    return;
                 from_jid = message.get('from_jid');
                 msgid = message.get('msgid');
                 let xml = message.get('xml');
