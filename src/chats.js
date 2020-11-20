e5dbc7a6bf3ea72ec2350db39bf644541e86ce33
Valeria Titova <valeria.nogina@redsolution.com>
2020-11-20 18:28:30 +0500

    Added .chat-day-indicator coloring in encrypted chat

--- src/chats.js
+++ src/chats.js
@@ -7121,6 +7121,7 @@ define("xabber-chats", function () {
                         let is_scrolled_bottom = this.view.isScrolledToBottom();
                         this.$el.attr('data-trust', is_trusted);
                         this.view.$('.chat-message:not([data-trust=untrusted])').attr('data-trust', is_trusted);
+                        this.view.$('.chat-day-indicator:not(.fixed-day-indicator-wrap)').attr('data-trust', is_trusted);
                         this.$el.removeClass('loading');
                         this.$el.children('.preloader-wrapper').detach();
                         if (is_trusted == 'none')
@@ -7147,6 +7148,7 @@ define("xabber-chats", function () {
                                 this.$el.attr('data-contact-trust', is_contact_trusted);
                             }
                             this.view.$('.chat-message:not([data-trust=untrusted])').attr('data-trust', is_contact_trusted);
+                            this.view.$('.chat-day-indicator:not(.fixed-day-indicator-wrap)').attr('data-trust', is_contact_trusted);
                             xabber.chat_body.updateHeight();
                             is_scrolled_bottom && this.view.scrollToBottom();
                             this.focusOnInput();
