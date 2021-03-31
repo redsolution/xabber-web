deedbc8c3a63576489cc51e7850b8ba47146dfc6
Valeria Titova <valeria.nogina@redsolution.com>
2021-03-31 18:09:05 +0500

    Improved prepare to build script

--- translations/xml_to_json.js
+++ translations/xml_to_json.js
@@ -1,26 +1,89 @@
 let fs = require('fs'),
     xmldom = require('xmldom'),
     json = {},
-    http = require('https');
-    DOMParser = new xmldom.DOMParser;
-let args = process.argv.slice(2),
+    http = require('https'),
+    DOMParser = new xmldom.DOMParser,
+    args = process.argv.slice(2),
     token = args[0],
     translation_progress = {};
-http.get({protocol: "https:", host: "crowdin.com", path: "/api/v2/projects/110652/languages/progress?limit=200", headers: {"Authorization": `Bearer ${token}`, "Content-Type": "application/json"}}, (res) => {
-    let rawData = "";
-    res.setEncoding('utf8');
-    res.on('data', (body) => {
-        rawData += body;
-    });
-    res.on('end', () => {
-        let json = JSON.parse(rawData);
-        json.data.forEach((lang) => {
-            translation_progress[lang.data.languageId] = lang.data.translationProgress;
+
+function loadTranslationsProgress () {
+    return new Promise((resolve, reject) => {
+        http.get({protocol: "https:", host: "crowdin.com", path: "/api/v2/projects/110652/languages/progress?limit=200", headers: {"Authorization": `Bearer ${token}`, "Content-Type": "application/json"}}, (res) => {
+            let rawData = "";
+            res.setEncoding('utf8');
+            res.on('data', (body) => {
+                rawData += body;
+            });
+            res.on('end', () => {
+                let json = JSON.parse(rawData);
+                json.data.forEach((lang) => {
+                    translation_progress[lang.data.languageId] = lang.data.translationProgress;
+                });
+                resolve();
+            });
         });
     });
-});
-/*http.get({protocol: "https:", host: "crowdin.com", path: "/api/v2/projects/110652/languages/progress?limit=200", headers: {"Authorization": `Bearer ${token}`, "Content-Type": "application/json"}}, (res) => {
-});*/
+}
+
+function downloadStrings () {
+    return new Promise((resolve, reject) => {
+        http.get({
+            protocol: "https:",
+            host: "crowdin.com",
+            path: "/api/v2/projects/110652/files",
+            headers: {"Authorization": `Bearer ${token}`, "Content-Type": "application/json"}
+        }, (res) => {
+            let rawData = "";
+            res.setEncoding('utf8');
+            res.on('data', (body) => {
+                rawData += body;
+            });
+            res.on('end', () => {
+                let json = JSON.parse(rawData),
+                    files_count = json.data.length;
+                json.data.forEach((lang) => {
+                    downloadFile(lang.data.id, lang.data.path).then(() => {
+                        !--files_count && resolve();
+                    });
+                });
+            });
+        });
+    });
+}
+
+function downloadFile (file_id, file_name) {
+    return new Promise((resolve, reject) => {
+        http.get({
+            protocol: "https:",
+            host: "crowdin.com",
+            path: `/api/v2/projects/110652/files/${file_id}/download`,
+            headers: {"Authorization": `Bearer ${token}`, "Content-Type": "application/json"}
+        }, (res) => {
+            let rawData = "";
+            res.setEncoding('utf8');
+            res.on('data', (body) => {
+                rawData += body;
+            });
+            res.on('end', () => {
+                http.get(JSON.parse(rawData).data.url, (response) => {
+                    let rawData = "";
+                    response.setEncoding('utf8');
+                    response.on('data', (body) => {
+                        rawData += body;
+                    });
+                    response.on('end', () => {
+                        let path = file_name.slice(0, file_name.lastIndexOf("/"));
+                        if (!fs.existsSync(`.${path}`))
+                            require('child_process').execSync(`mkdir -p -m 755 ".${path}"`);
+                        fs.writeFileSync(`.${file_name}`, rawData, {encoding: 'utf8'});
+                        resolve();
+                    });
+                });
+            });
+        });
+    });
+}
 
 function parseChildren (elem) {
     let childrens = elem.childNodes, str = "";
@@ -39,50 +102,76 @@ function parseChildren (elem) {
     }
     return str;
 }
-fs.readdirSync('./values').forEach(file => {
-    let xml = fs.readFileSync(`./values/${file}`, 'utf-8'),
-        document = DOMParser.parseFromString(xml),
-        strings = document.getElementsByTagName('string'),
-        plurals = document.getElementsByTagName('plurals');
-    for (let i = 0; i < strings.length; i++) {
-       let id = strings[i].getAttribute('name');
-        json[id] = parseChildren(strings[i]);
-    }
 
-    for (let i = 0; i < plurals.length; i++) {
-        let id =  plurals[i].getAttribute('name'),
-            items = plurals[i].getElementsByTagName('item');
-        for (let j = 0; j < items.length; j++) {
-            json[`${id}_plural_${j}`] = parseChildren(items[j]);
-        }
-    }
-});
-
-
-let all_languages = fs.readdirSync('./languages').filter(file => file.indexOf('values-') == 0);
-all_languages.forEach((f_name) =>{
-    let translations = {};
-    fs.readdirSync(`./languages/${f_name}`).forEach(file => {
-        let xml = fs.readFileSync(`./languages/${f_name}/${file}`, 'utf-8'),
+function convertStringsToJSON () {
+    fs.readdirSync('./values').forEach(file => {
+        let xml = fs.readFileSync(`./values/${file}`, 'utf-8'),
             document = DOMParser.parseFromString(xml),
             strings = document.getElementsByTagName('string'),
             plurals = document.getElementsByTagName('plurals');
         for (let i = 0; i < strings.length; i++) {
             let id = strings[i].getAttribute('name');
-            translations[id] = parseChildren(strings[i]);
+            json[id] = parseChildren(strings[i]);
         }
 
         for (let i = 0; i < plurals.length; i++) {
             let id =  plurals[i].getAttribute('name'),
                 items = plurals[i].getElementsByTagName('item');
             for (let j = 0; j < items.length; j++) {
-                translations[`${id}_plural_${j}`] = parseChildren(items[j]);
+                json[`${id}_plural_${j}`] = parseChildren(items[j]);
             }
         }
     });
-    fs.writeFileSync(`${f_name.slice(7)}.js`, `let translations = ${JSON.stringify(translations)}; typeof define === "function" && define(() => { return translations;});`);
+}
+
+function convertTranslationsToJSON () {
+    let all_languages = fs.readdirSync('./languages').filter(file => file.indexOf('values-') == 0);
+    all_languages.forEach((f_name) =>{
+        let translations = {};
+        fs.readdirSync(`./languages/${f_name}`).forEach(file => {
+            let xml = fs.readFileSync(`./languages/${f_name}/${file}`, 'utf-8'),
+                document = DOMParser.parseFromString(xml),
+                strings = document.getElementsByTagName('string'),
+                plurals = document.getElementsByTagName('plurals');
+            for (let i = 0; i < strings.length; i++) {
+                let id = strings[i].getAttribute('name');
+                translations[id] = parseChildren(strings[i]);
+            }
+
+            for (let i = 0; i < plurals.length; i++) {
+                let id =  plurals[i].getAttribute('name'),
+                    items = plurals[i].getElementsByTagName('item');
+                for (let j = 0; j < items.length; j++) {
+                    translations[`${id}_plural_${j}`] = parseChildren(items[j]);
+                }
+            }
+        });
+        fs.writeFileSync(`${f_name.slice(7)}.js`, `let translations = ${JSON.stringify(translations)}; typeof define === "function" && define(() => { return translations;});`);
+    });
+}
+
+function downloadTranslations () {
+    http.request({method: 'POST', protocol: "https:", host: "crowdin.com", path: "/api/v2/projects/110652/translations/builds", headers: {"Authorization": `Bearer ${token}`, "Content-Type": "application/json"}}, (res) => {
+        console.log(res);
+        let rawData = "";
+        res.setEncoding('utf8');
+        res.on('data', (body) => {
+            rawData += body;
+            console.log(body);
+        });
+        res.on('end', () => {
+            console.log(rawData);
+        });
+    });
+}
+// downloadTranslations();
+
+loadTranslationsProgress().then(() => {
+    return downloadStrings();
+}).then(() => {
+    console.log('Strings loaded.....');
+    convertStringsToJSON();
+
+    fs.writeFileSync(`translation_progress.js`, `let client_translation_progress = ${JSON.stringify(translation_progress)}; typeof define === "function" && define(() => { return client_translation_progress;});`);
+    fs.writeFileSync(`en_lang.js`, `let default_translation = ${JSON.stringify(json)}; typeof define === "function" && define(() => { return default_translation;});`);
 });
-
-
-fs.writeFileSync(`translation_progress.js`, `let client_translation_progress = ${JSON.stringify(translation_progress)}; typeof define === "function" && define(() => { return client_translation_progress;});`);
-fs.writeFileSync(`en_lang.js`, `let default_translation = ${JSON.stringify(json)}; typeof define === "function" && define(() => { return default_translation;});`);
