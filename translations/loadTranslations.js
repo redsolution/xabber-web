e58b3db01576e016fd79335298ece46987e57d47
Valeria Nogina <valeria.nogina@redsolution.ru>
2021-04-15 09:24:51 +0500

    Splitted scripts to update translations

--- translations/loadTranslations.js
+++ translations/loadTranslations.js
@@ -1,9 +1,6 @@
 let fs = require('fs'),
-    xmldom = require('xmldom'),
-    json = {},
     http = require('https'),
     unzip = require('unzip'),
-    DOMParser = new xmldom.DOMParser,
     args = process.argv.slice(2),
     token = args[0],
     _pending_finished,
@@ -87,73 +84,8 @@ function downloadFile (file_id, file_name) {
     });
 }
 
-function parseChildren (elem) {
-    let childrens = elem.childNodes, str = "";
-    for (let i = 0; i < childrens.length; i++) {
-        let flag = 0;
-        if (childrens[i].nodeName && (childrens[i].nodeName == 'b' || childrens[i].nodeName == 'i' || childrens[i].nodeName == 'u')) {
-            str += `<${childrens[i].nodeName}>`;
-            flag = 1;
-        }
-        if (childrens[i].childNodes && childrens[i].childNodes.length) {
-            str += parseChildren(childrens[i]);
-        } else if (childrens[i].nodeValue) {
-            str += childrens[i].nodeValue;
-        }
-        flag && (str += `</${childrens[i].nodeName}>`);
-    }
-    return str;
-}
-
-function convertStringsToJSON () {
-    fs.readdirSync('./values').forEach(file => {
-        let xml = fs.readFileSync(`./values/${file}`, 'utf-8'),
-            document = DOMParser.parseFromString(xml),
-            strings = document.getElementsByTagName('string'),
-            plurals = document.getElementsByTagName('plurals');
-        for (let i = 0; i < strings.length; i++) {
-            let id = strings[i].getAttribute('name');
-            json[id] = parseChildren(strings[i]);
-        }
-
-        for (let i = 0; i < plurals.length; i++) {
-            let id =  plurals[i].getAttribute('name'),
-                items = plurals[i].getElementsByTagName('item');
-            for (let j = 0; j < items.length; j++) {
-                json[`${id}_plural_${j}`] = parseChildren(items[j]);
-            }
-        }
-    });
-}
-
-function convertTranslationsToJSON () {
-    let all_languages = fs.readdirSync('./languages').filter(file => file.indexOf('values-') == 0);
-    all_languages.forEach((f_name) =>{
-        let translations = {};
-        fs.readdirSync(`./languages/${f_name}`).forEach(file => {
-            let xml = fs.readFileSync(`./languages/${f_name}/${file}`, 'utf-8'),
-                document = DOMParser.parseFromString(xml),
-                strings = document.getElementsByTagName('string'),
-                plurals = document.getElementsByTagName('plurals');
-            for (let i = 0; i < strings.length; i++) {
-                let id = strings[i].getAttribute('name');
-                translations[id] = parseChildren(strings[i]);
-            }
-
-            for (let i = 0; i < plurals.length; i++) {
-                let id =  plurals[i].getAttribute('name'),
-                    items = plurals[i].getElementsByTagName('item');
-                for (let j = 0; j < items.length; j++) {
-                    translations[`${id}_plural_${j}`] = parseChildren(items[j]);
-                }
-            }
-        });
-        fs.writeFileSync(`${f_name.slice(7)}.js`, `typeof define === "function" && define(() => { return ${JSON.stringify(translations)};});`);
-    });
-}
-
 function getTranslationsURL () {
-    console.log('Build translations.....');
+    console.log('Download translations.....');
     return new Promise((resolve, reject) => {
         let request = http.request({method: 'POST', protocol: "https:", hostname: "crowdin.com", path: "/api/v2/projects/110652/translations/builds", headers: {"Authorization": `Bearer ${token}`, "Content-Type": "application/json"}}, (res) => {
             let rawData = "";
@@ -218,19 +150,11 @@ getTranslationsURL().then((url) => {
     return downloadArchive(url);
 }).then(() => {
     console.log('Translations loaded.....');
-    console.log('Converting translations.....');
-    convertTranslationsToJSON();
-    console.log('***Translations written successfully***');
 });
 
 loadTranslationsProgress().then(() => {
     return downloadStrings();
 }).then(() => {
     console.log('Strings loaded.....');
-    console.log('Converting strings.....');
-    convertStringsToJSON();
-    console.log('***Strings written successfully***');
-
     fs.writeFileSync(`translation_progress.js`, `let client_translation_progress = ${JSON.stringify(translation_progress)}; typeof define === "function" && define(() => { return client_translation_progress;});`);
-    fs.writeFileSync(`en.js`, `typeof define === "function" && define(() => { return ${JSON.stringify(json)};});`);
 });
