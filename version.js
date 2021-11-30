9953e5ec6df545a69eeb0c3f414cfbb845df0128
egor.merkushkin <egor.merkushkin@redsolution.ru>
2021-11-30 10:21:57 +0500

    Version and bundle

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.4","version_description":"Added string, updated strings, fixed last message for forward of non group chats"}'
+'{"version_number":"2.3.2.5","version_description":"New sync"}'
 )});
