b30f14c280187c6b4845e8ea1b4ab555002ed364
egor.merkushkin <egor.merkushkin@redsolution.ru>
2022-03-05 12:29:05 +0500

    Develop version and build for new device token and redesign with various fixes

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.2.5","version_description":"New sync"}'
+'{"version_number":"2.3.2.6","version_description":"Token fix,\n redesign,\n fixes"}'
 )});
