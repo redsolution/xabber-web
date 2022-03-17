5e63a0ba1b7cc45dfa3a925328bb71c8ac9cc897
egor.merkushkin <egor.merkushkin@redsolution.ru>
2022-03-17 17:45:01 +0500

    Registration testing build with applied changes to token counter

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.2.8","version_description":"Registration testing"}'
+'{"version_number":"2.3.2.9","version_description":"Registration testing build with applied changes to token counter"}'
 )});
