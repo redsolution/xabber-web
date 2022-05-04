9dea1e963e02bad0b161efc3ab94316fa50e5b6b
egor.merkushkin <egor.merkushkin@redsolution.ru>
2022-05-04 16:11:24 +0500

    registration design update 2

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.2.16","version_description":"Registration testing build"}'
+'{"version_number":"2.3.2.17","version_description":"Registration testing build"}'
 )});
