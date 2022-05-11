324599203d56b160c52c000efa18e0d07a42beef
egor.merkushkin <egor.merkushkin@redsolution.ru>
2022-05-11 16:28:51 +0500

    registration build

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.2.22","version_description":"made that sign up is opening if rkey is set"}'
+'{"version_number":"2.3.2.23","version_description":"omemo empty fingerprint fix"}'
 )});
