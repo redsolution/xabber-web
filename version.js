77a3b84bbf685e0d9ee59a6c981f3ddd913115c8
egor.merkushkin <egor.merkushkin@redsolution.ru>
2022-05-13 15:04:19 +0500

    omemo device token id registration build

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.2.26","version_description":"Registration error text from server response"}'
+'{"version_number":"2.3.2.27","version_description":"omemo device token id registration build"}'
 )});
