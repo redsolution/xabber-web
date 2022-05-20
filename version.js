83ae403d94d296886ae76d0bc2faf3bccee5ff3e
egor.merkushkin <egor.merkushkin@redsolution.ru>
2022-05-20 15:34:37 +0500

    omemo device label update

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.2.33","version_description":"added focus to add contact and create groupchat inputs on render, added highlights to errored input in create groupchat, updated translations, minor changes to contact details circles"}'
+'{"version_number":"2.3.2.34","version_description":"omemo update"}'
 )});
