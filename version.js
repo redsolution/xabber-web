774e0b32b2a9d81177c0b4dc7f2eed18eeddbc62
egor.merkushkin <egor.merkushkin@redsolution.ru>
2022-07-05 15:11:36 +0500

    client id caching, connection conflict handling, modal dialog width change

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.2.60","version_description":"fixed mentions and quotes disappearing on edit message, added mention button in message text formatting, added avatar shape option in settings"}'
+'{"version_number":"2.3.2.61","version_description":"client id caching, connection conflict handling, modal dialog width change"}'
 )});
