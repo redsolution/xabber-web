66b7c63248860f6e823092323faaf77cace1a695
egor.merkushkin <egor.merkushkin@redsolution.ru>
2022-08-04 14:09:44 +0500

    fixed blocking buttons text color, added features to account server features

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.2.73","version_description":"removed incoming subscription system message, added subscription button for incoming subscription, changed line breaker for quill textarea text pasting"}'
+'{"version_number":"2.3.2.74","version_description":"fixed blocking buttons text color, added features to account server features"}'
 )});
