f47cf50a79820a689ed24dd7fcce098d9ee4ddad
egor.merkushkin <egor.merkushkin@redsolution.ru>
2022-09-06 15:52:36 +0500

    fixed authfail handling after reconnections, fixed subscription request out attribute not changing on roster item, added setting gallery auth attribute to false on getting mediagallery feature, changed contact subscription button translation

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.2.90","version_description":"added sync presence handling"}'
+'{"version_number":"2.3.2.91","version_description":"fixed authfail handling after reconnections, fixed subscription request out attribute not changing on roster item, added setting gallery auth attribute to false on getting mediagallery feature, changed contact subscription button translation"}'
 )});
