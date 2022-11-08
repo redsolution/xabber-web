6d8749467dd29101fbc16c5107689aca6dc1eb98
egor.merkushkin <egor.merkushkin@redsolution.ru>
2022-11-08 15:58:01 +0500

    fixed avatars positions in chat

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.2.146","version_description":"chat message input and chat messages redesign, fixed img onload error on ogp reference rendering"}'
+'{"version_number":"2.3.2.147","version_description":"fixed avatars positions in chat"}'
 )});
