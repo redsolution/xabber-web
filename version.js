5c912c0abcf09f50389b1b5dfdebe86ebed7d601
egor.merkushkin <egor.merkushkin@redsolution.ru>
2022-11-09 18:17:28 +0500

    changed speed and reversed pulsation of link reference loading

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.2.148","version_description":"fixed message attachments preview container appearing when there is no attachments, changed link reference loading button appearance, fixed message input appearance in encrypted chats and normal chats"}'
+'{"version_number":"2.3.2.149","version_description":"changed speed and reversed pulsation of link reference loading"}'
 )});
