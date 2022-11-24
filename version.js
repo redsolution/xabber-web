436b18e53ef991144d94e6d98e5468f56fa46454
egor.merkushkin <egor.merkushkin@redsolution.ru>
2022-11-24 15:20:10 +0500

    fixed plyr iframe loading height jumping

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.2.162","version_description":"changed chat head layout for fitting media player and contact name, changed media player sizing depending on chat head width"}'
+'{"version_number":"2.3.2.163","version_description":"fixed plyr iframe loading height jumping"}'
 )});
