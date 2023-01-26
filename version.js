4c9e700d775ca306505fe8eb5caff37811dac8a3
egor.merkushkin <egor.merkushkin@redsolution.ru>
2023-01-26 10:40:34 +0500

    Webpack merge

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.2.205","version_description":"fixed chat reading on getting system message with from attribute is this account jid, which was causing displayed sending in this chat when that type of messages was coming from archive request"}'
+'{"version_number":"2.3.3.1","version_description":"Webpack merge"}'
 )});
