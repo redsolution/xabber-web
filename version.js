62e53d4cc6274ce4ffddee3d4ffc1669bf474a40
egor.merkushkin <egor.merkushkin@redsolution.ru>
2023-05-05 14:12:26 +0500

    fixed empty body messages visuals in chat content

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.3.96","version_description":"fixed adding messages to chat content html if there were unrendered messages before it"}'
+'{"version_number":"2.3.3.97","version_description":"fixed empty body messages visuals in chat content"}'
 )});
