9c38746ed3696e45880cd96b69dd328775122cc6
egor.merkushkin <egor.merkushkin@redsolution.ru>
2023-06-01 12:12:42 +0500

    fixed loop of loading images

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.3.115","version_description":"changed message reading, now it reads them when their bottom is visible"}'
+'{"version_number":"2.3.3.116","version_description":"fixed loop of loading images"}'
 )});
