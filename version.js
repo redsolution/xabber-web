0488e906e55a439a3b306ef3b9fcb29b8ecba40a
egor.merkushkin <egor.merkushkin@redsolution.ru>
2023-06-21 17:43:02 +0500

    fixed toolbar with circle avatars

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.3.132","version_description":"fixed omemo support for XEP-Devices, toolbar redesign"}'
+'{"version_number":"2.3.3.133","version_description":"fixed toolbar with circle avatars"}'
 )});
