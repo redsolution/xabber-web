ba56252cae621590fc115dbc7ea349cf5d1b1392
egor.merkushkin <egor.merkushkin@redsolution.ru>
2023-08-29 14:21:35 +0500

    fixed timestamp updating in chat items when there is active draft, changed unverified devices dialog text

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.3.187","version_description":"fixed that removed contact became groupchat on sync deleted push"}'
+'{"version_number":"2.3.3.188","version_description":"fixed timestamp updating in chat items when there is active draft, changed unverified devices dialog text"}'
 )});
