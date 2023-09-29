5769ede4a411fd81b0f6f11bb516873c817177c5
egor.merkushkin <egor.merkushkin@redsolution.ru>
2023-09-29 15:37:58 +0500

    added varying lock icons to identity verification button

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.4.21","version_description":"fixed empty ids archive request, fixed private chat invite screen on opening through participant details"}'
+'{"version_number":"2.3.4.22","version_description":"added varying lock icons to identity verification button"}'
 )});
