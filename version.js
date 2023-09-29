04ddd00d22614b752b9d595543c9d22397bdd7cf
egor.merkushkin <egor.merkushkin@redsolution.ru>
2023-09-29 16:21:13 +0500

    changed visuals of checkbox-field, removed settings button from toolbar

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.4.22","version_description":"added varying lock icons to identity verification button"}'
+    '{"version_number":"2.3.4.23","version_description":"changed visuals of checkbox-field, removed settings button from toolbar"}'
 )});
