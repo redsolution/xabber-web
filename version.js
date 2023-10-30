acd632db783ccfbde173c94c2ea2458040bd3da0
egor.merkushkin <egor.merkushkin@redsolution.ru>
2023-10-30 10:58:28 +0500

    fixed clickable placeholder in group create view

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.4.55","version_description":"changed modal dialog options forming, changed translations in change language form, changed translations text size and line height"}'
+'{"version_number":"2.3.4.56","version_description":"fixed clickable placeholder in group create view"}'
 )});
