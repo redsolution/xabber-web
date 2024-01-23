82db6b189c5311ea54ada80530059e921f81acf2
egor.merkushkin <egor.merkushkin@redsolution.ru>
2024-01-23 11:40:50 +0500

    fixed assets prefix context for emoji font

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.4.123","version_description":"fixed assets prefix for emoji font"}'
+'{"version_number":"2.3.4.124","version_description":"fixed assets prefix context for emoji font"}'
 )});
