7fc2d9aef0bbb3d510fc5d0579261bb62e92775d
egor.merkushkin <egor.merkushkin@redsolution.ru>
2024-03-19 14:37:50 +0500

    fixed msg retraction in saved msgs chat

--- src/chats.js
+++ src/chats.js
@@ -1912,7 +1912,7 @@ xabber.EphemeralTimerSelector = xabber.BasicView.extend({
                     .c('retract-message', {
                         id: (this.get('group_chat') && contact_stanza_id || stanza_id),
                         xmlns: Strophe.NS.REWRITE,
-                        conversation: this.contact.get('jid'),
+                        conversation: this.account.server_features.get(Strophe.NS.XABBER_FAVORITES) && this.get('saved') ? this.account.server_features.get(Strophe.NS.XABBER_FAVORITES).get('from') : this.contact.get('jid'),
                         symmetric: symmetric,
                         type: this.get('sync_type') ? this.get('sync_type') : this.getConversationType(this),
                         by: this.account.get('jid')
--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.4.168","version_description":"fixed headline msg parsing trying to get contact with own jid in from attribute of headline"}'
+'{"version_number":"2.3.4.169","version_description":"fixed msg retraction in saved msgs chat"}'
 )});
