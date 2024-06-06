6f029e11c9aa056112fc25411fcddcd6871a2ec5
egor.merkushkin <egor.merkushkin@redsolution.ru>
2024-06-06 15:52:34 +0500

    changed design of active session modal

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.5.27","version_description":"fixed date-indicator color in encrypted chat"}'
+'{"version_number":"2.3.5.28","version_description":"changed design of active session modal"}'
 )});
