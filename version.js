9aecf3f71dc5b55825f505fc296d9f5fcabd2ffd
egor.merkushkin <egor.merkushkin@redsolution.ru>
2024-06-27 16:54:54 +0500

    fixed hover of button in fingerprints, added pulsating of background to enter code step of session card in chat, added fingerprints icon coloring depending on trust

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.3.5.50","version_description":"updated text for contacts verification session"}'
+'{"version_number":"2.3.5.51","version_description":"fixed hover of button in fingerprints, added pulsating of background to enter code step of session card in chat, added fingerprints icon coloring depending on trust"}'
 )});
