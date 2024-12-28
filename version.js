fd08222eeca36d8afd82842678b910d14cbf745b
egor.merkushkin <egor.merkushkin@redsolution.ru>
2024-12-28 18:19:21 +0500

    fixed search width in tabs

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.4.38","version_description":"added hiding search for tabs, fixed account selector error in modals with account selector, changed desktop notification click on invitation to open notifications"}'
+'{"version_number":"2.4.39","version_description":"fixed search width in tabs "}'
 )});
