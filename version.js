2a3958cf45b19d1f84e6e2de17ca29d382c90e2d
egor.merkushkin <egor.merkushkin@redsolution.ru>
2024-12-28 18:31:10 +0500

    fixed search search icon fill

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.4.40","version_description":"fixed search search icon fill"}'
+'{"version_number":"2.4.41","version_description":"fixed search search icon fill"}'
 )});
