26d9321441fcc9f69ccdb615104a68a4d60044c8
egor.merkushkin <egor.merkushkin@redsolution.ru>
2025-06-25 17:25:02 +0500

    contacts groups redesign - changed font size

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.4.124","version_description":"contacts groups redesign, changed avatar setting if proxy url got error 404"}'
+'{"version_number":"2.4.125","version_description":"contacts groups redesign"}'
 )});
