29dcf7be4c7af196b4898033e9649655d44e3448
egor.merkushkin <egor.merkushkin@redsolution.ru>
2025-06-27 18:08:01 +0500

    changed toolbar and groupchats tab icons

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.4.131","version_description":"changed contacts icon"}'
+'{"version_number":"2.4.132","version_description":"changed toolbar and groupchats tab icons"}'
 )});
