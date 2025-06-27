1f19b12148660b04665a7052f96bee57baf9756e
egor.merkushkin <egor.merkushkin@redsolution.ru>
2025-06-27 17:29:50 +0500

    changed contacts icon

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.4.130","version_description":"contacts tab fixes and changes"}'
+'{"version_number":"2.4.131","version_description":"changed contacts icon"}'
 )});
