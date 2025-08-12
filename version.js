c88babc6a6b44f251be5ea8c0d17f5ac32b6b142
egor.merkushkin <egor.merkushkin@redsolution.ru>
2025-08-12 15:24:32 +0500

    changed contacts visuals in chats search and forward messages search

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"2.4.178","version_description":"changed new trusted device message checking"}'
+'{"version_number":"2.4.179","version_description":"changed contacts visuals in chats search and forward messages search"}'
 )});
