a046aa52f28a12ed88c288f4460f0e5e2abff129
egor.merkushkin <egor.merkushkin@redsolution.ru>
2026-03-12 10:40:26 +0500

    Updated moment package version

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 define(function () { return JSON.parse(
-'{"version_number":"26.03","version_description":"Latest major release of Xabber Web"}'
+'{"version_number":"26.03.2","version_description":"Updated moment package version"}'
 )});
